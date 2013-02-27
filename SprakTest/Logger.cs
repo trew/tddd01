48484c7612868898883c213e411748cb9e94d162
Samuel Andersson <smulster@gmail.com>
2013-02-27 10:44:27 +0100

    Find unique file to save to. Max 100 files

--- SprakTest/Logger.cs
+++ SprakTest/Logger.cs
@@ -8,18 +8,55 @@ namespace SprakTest
 {
     class Logger
     {
-        public void save(string name, int age, bool male, Test test)
-        {
+        private string getFileName(string name, int age, bool male, int id) {
             string fileName = name.Split('\\')[1].Split('.')[0];
             fileName += "_";
             fileName += male ? "M" : "F";
             fileName += age;
-            fileName += "_";
-            fileName += DateTime.Now.Millisecond;
+            if (id > 0) {
+                fileName += "_" + id;
+            }
             fileName += ".txt";
+            return fileName;
+        }
+
+        private KeyValuePair<string, FileStream> getFileStream(string name, int age, bool male)
+        {
+            int id = 0;
+            string fileName;
+            FileStream fs;
+            while (true)
+            {
+                try
+                {
+                    fileName = getFileName(name, age, male, id);
+                    fs = File.Open(Directory.GetCurrentDirectory() + "/" + fileName, FileMode.CreateNew);
+                    return new KeyValuePair<string,FileStream>(fileName, fs);
+                }
+                catch (IOException e)
+                {
+                    if (e.Message.EndsWith("already exists.") && id < 100)
+                    {
+                        id++;
+                    }
+                    else
+                        return new KeyValuePair<string, FileStream>();
+                }
+            }
+        }
+
+        public void save(string name, int age, bool male, Test test)
+        {
             try
             {
-                FileStream fs = File.Open(Directory.GetCurrentDirectory() + "/" + fileName, FileMode.CreateNew);
+                KeyValuePair<string, FileStream> filePair = getFileStream(name, age, male);
+                if (filePair.Key.Length == 0)
+                {
+                    Console.WriteLine("Cannot write results.");
+                    return;
+                }
+                string fileName = filePair.Key;
+                FileStream fs = filePair.Value;
                 fs.Close();
                 fs = File.OpenWrite(fileName);
                 StreamWriter sw = new StreamWriter(fs);
